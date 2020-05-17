18a6a5263627c7854be4e1a1a961242b286af67c
andyburton0912 <65254246+andyburton0912@users.noreply.github.com>
2020-05-17 23:24:59 +0100

    Day 2 Added

---
+++ Day-1-data-types/solution.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+number = int(input())
+fl_number = float(input())
+words = input()
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print(i + number)
+# Print the sum of the double variables on a new line.
+print(d + fl_number)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + words)
